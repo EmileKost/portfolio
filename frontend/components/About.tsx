b3bba97cc0aedfdbac79339c4a847b4dc2746500
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-01 17:09:42 +0200

    feat: about

--- frontend/components/About.tsx
+++ frontend/components/About.tsx
@@ -8,7 +8,7 @@ const textItems = [
 ];
 export const About = () => {
 	return (
-		<div className="h-screen w-full bg-blue-primary px-2 md:px-6 flex flex-col items-end md:items-start">
+		<div className="h-screen w-full bg-blue-primary px-2 md:px-6 flex flex-col justify-end md:justify-start">
 			{textItems && (
 				<div className="mb-[120px] md:mb-0 md:mt-[250px]">
 					<h2 className="mt-0 flex flex-col gap-2 md:gap-4 uppercase mb-2 md:mb-4">
@@ -23,11 +23,11 @@ export const About = () => {
 							</span>
 						))}
 					</h2>
+					<button className="font-primary font-normal text-white-primary text-3xl sm:text-4xl md:text-5xl lg:text-[80px] uppercase">
+						projects
+					</button>
 				</div>
 			)}
-			<button className="font-primary font-normal text-white-primary text-3xl sm:text-4xl md:text-5xl lg:text-[80px] uppercase">
-				projects
-			</button>
 		</div>
 	);
 };
