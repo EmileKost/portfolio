6acd5b561496f2cae470fd6e957b73894e15ac5c
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-01 16:37:36 +0200

    feat: wip function

--- frontend/components/HomeHero.tsx
+++ frontend/components/HomeHero.tsx
@@ -4,6 +4,8 @@ import { twMerge } from "tailwind-merge";
 import { motion } from "framer-motion";
 
 import { MovingImage } from "./MovingImage";
+import { useRef } from "react";
+import { getWindowSizes } from "@/src/utils/getWindowSizes";
 
 const heroText = ["Hi there,", "I love to", "develop", "websites"];
 const spanVariants = {
@@ -19,8 +21,28 @@ const spanVariants = {
 };
 
 export const HomeHero = () => {
+	const refContainer = useRef<HTMLDivElement>(null);
+	const getMousePosition = (e: MouseEvent) => {
+		if (!refContainer.current || !e) return;
+
+		const { windowWidth, windowHeight } = getWindowSizes();
+		const { clientX, clientY } = e;
+
+		if (!windowWidth || !windowHeight) return;
+
+		// Check which half mouse is
+		if (clientX < windowWidth / 2) {
+			// Logic here
+		} else {
+			// Logic here
+		}
+	};
+
 	return (
-		<div className="w-full relative h-screen flex items-start md:items-center">
+		<div
+			onMouseMove={(e) => getMousePosition(e)}
+			ref={refContainer}
+			className="w-full relative h-screen flex items-start md:items-center">
 			<h1 className="flex flex-col gap-2 md:gap-6 mt-[150px] md:mt-0">
 				{heroText &&
 					heroText.map((text: string, idx: number) => (
