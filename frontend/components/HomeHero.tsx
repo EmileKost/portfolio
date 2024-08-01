033049c33a13d6b1a68b981e3a5c47c4f918b19b
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-01 12:39:30 +0200

    feat: styling of hero and image

--- frontend/components/HomeHero.tsx
+++ frontend/components/HomeHero.tsx
@@ -1,23 +1,26 @@
 import { twMerge } from "tailwind-merge";
 
+import { MovingImage } from "./MovingImage";
+
 const heroText = ["Hi there,", "I love to", "develop", "websites"];
 
 export const HomeHero = () => {
 	return (
-		<div className="w-full relative h-screen flex items-center">
-			<h1 className="flex flex-col gap-0 md:gap-6">
+		<div className="w-full relative h-screen flex items-start md:items-center">
+			<h1 className="flex flex-col gap-2 md:gap-6 mt-[150px] md:mt-0">
 				{heroText &&
 					heroText.map((text: string, idx: number) => (
 						<span
 							key={text}
 							className={twMerge(
-								"text-blue-primary block text-5xl sm:text-[60px] md:text-[140px] font-normal font-primary uppercase",
+								"text-blue-primary block text-5xl sm:text-[60px] md:text-[90px] lg:text-[140px] font-normal font-primary uppercase",
 								idx % 2 != 0 ? "ml-8 md:ml-[95px]" : "ml-0"
 							)}>
 							{text}
 						</span>
 					))}
 			</h1>
+			<MovingImage />
 		</div>
 	);
 };
---
+++ frontend/components/MovingImage.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+// TODO: Change IMG from hardcoded to CMS data and TYPES!!!
+export const MovingImage = ({ refMouse }: { refMouse?: any }) => {
+	return (
+		<figure className="absolute right-0 z-[-10] translate-y-0 bottom-0 md:bottom-1/2 md:translate-y-1/2">
+			<Image
+				src={"/images/me.png"}
+				alt="Picture of me"
+				width={490}
+				height={653}
+				className="w-[260px] h-[346px] sm:w-[300px] sm:h-[400px] md:w-[346px] md:h-[461px] lg:w-[490px] lg:h-[653px]"
+			/>
+			<figcaption className="text-base md:text-2xl text-blue-primary uppercase w-full text-end">
+				PS, Yes I am a nerd
+			</figcaption>
+		</figure>
+	);
+};
