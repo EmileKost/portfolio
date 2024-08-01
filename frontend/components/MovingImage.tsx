557e502d9ae82784c5ee37ea9d0a204795bee0fe
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-01 16:22:45 +0200

    feat: header animation

--- frontend/components/MovingImage.tsx
+++ frontend/components/MovingImage.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const movingImageVariants = {
 	initial: {
 		opacity: 0,
-		scale: 0.5,
+		scale: 0,
 		y: "50%",
 	},
 	animate: {
--- frontend/components/header/TheHeader.tsx
+++ frontend/components/header/TheHeader.tsx
@@ -1,14 +1,33 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { Menu } from "./Menu";
+
+const headerVariants = {
+	initial: {
+		opacity: 0,
+		y: -15,
+	},
+	animate: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.5,
+		},
+	},
+};
 
 export const TheHeader = () => {
 	return (
-		<header className="z-max px-2 md:px-6 py-14 flex justify-between items-start md:items-center font-primary w-full fixed top-0 left-0">
+		<motion.header
+			variants={headerVariants}
+			initial={"initial"}
+			animate={"animate"}
+			className="z-max px-2 md:px-6 py-14 flex justify-between items-start md:items-center font-primary w-full fixed top-0 left-0">
 			<h1 className="uppercase text-xl md:text-2xl text-blue-primary">
 				Emile Kost
 			</h1>
 			<Menu />
-		</header>
+		</motion.header>
 	);
 };
