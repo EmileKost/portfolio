75089354bbdaa330178a4398a4ceef6ca2628c1a
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-01 12:53:13 +0200

    feat: animating words

--- frontend/components/HomeHero.tsx
+++ frontend/components/HomeHero.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 import { MovingImage } from "./MovingImage";
 
 const heroText = ["Hi there,", "I love to", "develop", "websites"];
+const spanVariants = {
+	evenInitial: {
+		x: -30,
+	},
+	oddInitial: {
+		x: 30,
+	},
+	animate: {
+		x: 0,
+	},
+};
 
 export const HomeHero = () => {
 	return (
@@ -12,14 +24,17 @@ export const HomeHero = () => {
 			<h1 className="flex flex-col gap-2 md:gap-6 mt-[150px] md:mt-0">
 				{heroText &&
 					heroText.map((text: string, idx: number) => (
-						<span
+						<motion.span
 							key={text}
+							variants={spanVariants}
+							initial={idx % 2 != 0 ? "oddInitial" : "evenInitial"}
+							animate={"animate"}
 							className={twMerge(
 								"text-blue-primary block text-5xl sm:text-[60px] md:text-[90px] lg:text-[140px] font-normal font-primary uppercase",
 								idx % 2 != 0 ? "ml-8 md:ml-[95px]" : "ml-0"
 							)}>
 							{text}
-						</span>
+						</motion.span>
 					))}
 			</h1>
 			<MovingImage />
