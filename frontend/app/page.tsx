43f89f6548743b52f46578c41a7ca03f63b86c99
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-07 13:28:05 +0200

    feat: adding routes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,22 @@ import { TheHeader } from "@/components/header/TheHeader";
 import { HomeHero } from "@/components/HomeHero";
 import { About } from "@/components/About";
 
-import { useInView } from "framer-motion";
+import { ROUTES } from "@/src/routes/routes";
 
 export default function Page() {
+	const getProjects = async () => {
+		try {
+			const response = await fetch(`http://localhost:4500/api/projects`);
+			const result = await response.json();
+			console.log({ result });
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	};
+
+	getProjects();
+
 	return (
 		<>
 			<TheHeader />
