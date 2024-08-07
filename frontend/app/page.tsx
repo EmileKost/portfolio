79770356ddfbbe0b5bca329e1c8d2775e2036e24
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-07 13:39:47 +0200

    feat: framer package, whitelisting csfr and more shizle

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,9 +7,11 @@ import { ROUTES } from "@/src/routes/routes";
 export default function Page() {
 	const getProjects = async () => {
 		try {
-			const response = await fetch(`http://localhost:4500/api/projects`);
+			const response = await fetch(
+				`http://localhost:4500/api/test/66b35ab9fa1a7d0f20f3acf5`
+			);
 			const result = await response.json();
-			console.log({ result });
+			console.log(result);
 		} catch (err) {
 			console.log(err);
 			throw err;
