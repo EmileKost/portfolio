494c8f104f0bca4dcebde19655eb41a936c5730d
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-07 13:49:20 +0200

    feat: solving issue for now

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,21 +5,6 @@ import { About } from "@/components/About";
 import { ROUTES } from "@/src/routes/routes";
 
 export default function Page() {
-	const getProjects = async () => {
-		try {
-			const response = await fetch(
-				`http://localhost:4500/api/test/66b35ab9fa1a7d0f20f3acf5`
-			);
-			const result = await response.json();
-			console.log(result);
-		} catch (err) {
-			console.log(err);
-			throw err;
-		}
-	};
-
-	getProjects();
-
 	return (
 		<>
 			<TheHeader />
